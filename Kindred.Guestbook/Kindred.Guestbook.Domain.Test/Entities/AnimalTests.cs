7d997ae49a1ff92bdbf27a3f7224cb83a8ff1211
Dani <danielbahe@gmail.com>
2022-11-26 16:28:34 +0100

    Complete animals unit tests

--- Kindred.Guestbook/Kindred.Guestbook.Domain.Test/Entities/AnimalTests.cs
+++ Kindred.Guestbook/Kindred.Guestbook.Domain.Test/Entities/AnimalTests.cs
@@ -1,5 +1,7 @@
 using Kindred.Guestbook.Domain.Commands.Animals.CreateAnimal;
+using Kindred.Guestbook.Domain.Commands.Animals.UpdateAnimal;
 using Kindred.Guestbook.Domain.Entities;
+using System.Xml.Linq;
 using Xunit;
 
 namespace Kindred.Guestbook.Domain.Test.Entities
@@ -12,23 +14,21 @@ namespace Kindred.Guestbook.Domain.Test.Entities
         [InlineData("Lua", Species.Cat, "", true, true)]
         [InlineData("Lua", Species.Cat, "Is cute", true, true)]
         [InlineData("", Species.None, "", true, false)]
-        [InlineData("Lua", null, "Is cute", true, false)]
+        [InlineData("Lua", Species.None, "Is cute", true, false)]
         [InlineData("", Species.None, "Is cute", true, false)]
         [InlineData("Lua", Species.None, "", true, false)]
         [InlineData(null, Species.Cat, null, true, true)]
         [InlineData(null, Species.Cat, "Is cute", true, true)]
         [InlineData("Lua", Species.Cat, null, true, true)]
-        [InlineData(null, null, null, true, false)]
-        [InlineData("Lua", null, "Is cute", true, false)]
-        [InlineData(null, null, "Is cute", true, false)]
-        [InlineData("Lua", null, null, true, false)]
-        [InlineData("Lua", Species.Cat, null, true, true)]
+        [InlineData(null, Species.None, null, true, false)]
+        [InlineData(null, Species.None, "Is cute", true, false)]
+        [InlineData("Lua", Species.None, null, true, false)]
         [InlineData("Lua", Species.Cat, "Is cute", false, false)]
 
         public void CreateAnimalSuccessfully(string name, Species species, string description, bool validShelter, bool isSuccess)
         {
             var shelterId = validShelter ? Guid.NewGuid() : Guid.Empty;
-            var createAnimalCommand = new CreateAnimalCommandRequest(name, species, description, shelterId, ValueObjects.AnimalStatusValue.None);
+            var createAnimalCommand = new CreateAnimalCommandRequest(name, species, description, shelterId, ValueObjects.AnimalStatusValue.Lost);
 
             var animal = Animal.Create(createAnimalCommand);
 
@@ -47,6 +47,33 @@ namespace Kindred.Guestbook.Domain.Test.Entities
             Assert.Equal("Is Cute", animal.Value.Description);
             Assert.True(ValueObjects.AnimalStatusValue.Lost == animal.Value.Status.Status);
             Assert.NotEqual(Guid.Empty, animal.Value.ShelterId);
+        }
+
+        [Fact]
+        public void ShouldUpdateDataProperly()
+        {
+            var createAnimalCommand = new CreateAnimalCommandRequest("Lua", Species.Cat, "Is cute", Guid.NewGuid(), ValueObjects.AnimalStatusValue.Lost);
+            var animal = Animal.Create(createAnimalCommand);
+
+            var updateAnimalCommand = new UpdateAnimalCommandRequest(animal.Value.Id, "Leo", Species.Dog, "Is funny", ValueObjects.AnimalStatusValue.Found);
+            animal.Value.Update(updateAnimalCommand);
+
+            Assert.True(animal.IsSuccess);
+            Assert.NotEqual(animal.Value.Name, createAnimalCommand.Name);
+            Assert.NotEqual(animal.Value.Species, createAnimalCommand.Species);
+            Assert.NotEqual(animal.Value.Description, createAnimalCommand.Description);
+            Assert.False(animal.Value.Status.Status == createAnimalCommand.Status);
+        }
+
+        [Fact]
+        public void BeSoftDeleted()
+        {
+            var createAnimalCommand = new CreateAnimalCommandRequest("Lua", Species.Cat, "Is Cute", Guid.NewGuid(), ValueObjects.AnimalStatusValue.Lost);
+            var animal = Animal.Create(createAnimalCommand);
+
+            animal.Value.Delete();
+
+            Assert.True(animal.Value.IsDeleted);
         }
     }
 }
